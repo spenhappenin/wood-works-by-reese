2068002f1eefcc38a890dd87071325e19af8a42c
Spencer Richards <spencer.richards7@gmail.com>
2020-06-22 15:46:47 -0600

    remove unused variables

--- src/components/custom-work/CustomWorkPage.js
+++ src/components/custom-work/CustomWorkPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import styled from 'styled-components';
 
-import heroImage from '../../images/customWork/custom-work-hero.jpg';
+// import heroImage from '../../images/customWork/custom-work-hero.jpg';
 import CustomWorksSection from './CustomWorksSection';
 import GallerySection from './GallerySection';
 import { Br, } from '../../styles/shared';
